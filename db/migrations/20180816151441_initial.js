027d3abbffd0797f54c86316228306442e2c8834
Tyler Westlie <tyler.westlie@gmail.com>
2018-08-18 16:03:13 -0600

    migrated db, seeded meals to the db, created a meals endpoint

--- db/migrations/20180816151441_initial.js
+++ db/migrations/20180816151441_initial.js
@@ -1,8 +1,35 @@
 
 exports.up = function(knex, Promise) {
-  
+  return Promise.all([
+    knex.schema.createTable('meals', function(table) {
+      table.increments('id').primary();
+      table.string('name');
+
+      table.timestamps(true, true);
+    }),
+
+    knex.schema.createTable('foods', function(table) {
+      table.increments('id').primary();
+      table.string('name');
+      table.integer('calories');
+
+      table.timestamps(true, true);
+    }),
+
+    knex.schema.createTable('meal_foods', function(table) {
+      table.increments('id').primary();
+      table.integer('meal_id').references('id').inTable('meals').notNull()
+      table.integer('food_id').references('id').inTable('foods').notNull()
+
+      table.timestamps(true, true);
+    })
+  ])
 };
 
 exports.down = function(knex, Promise) {
-  
+  return Promise.all([
+     knex.schema.dropTable('meal_foods'),
+     knex.schema.dropTable('foods'),
+     knex.schema.dropTable('meals')
+   ]);
 };
--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('port', process.env.PORT || 3000);
@@ -11,9 +15,18 @@ app.get('/', (request, response) => {
   response.send('Welcome to Quantified Self With Express!');
 });
 
+app.get('/api/v1/meals', (request, response) => {
+  database('meals').select()
+    .then((meals) => {
+      response.status(200).json(meals);
+    })
+    .catch((error) => {
+      response.status(500).json({ error });
+    });
+});
+
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
 });
-
 
 module.exports = app;
