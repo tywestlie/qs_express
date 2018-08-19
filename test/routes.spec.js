4fbe96760ae0f98581739db65bff9cd258ceee98
Tyler Westlie <tyler.westlie@gmail.com>
2018-08-19 16:08:41 -0600

    GET /api/v1/meals queries all meals in the database

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -92,4 +92,20 @@ describe('API Routes', () => {
           });
       });
     });
+
+    describe('GET /api/v1/meals', () => {
+      it('queries all meals', done => {
+        chai.request(server)
+          .get('/api/v1/meals')
+          .end((err, response) => {
+            response.should.have.status(200);
+            response.should.be.json;
+            response.body.should.be.a('array');
+            response.body.length.should.equal(4);
+            response.body[0].should.have.property('name');
+            response.body[0].name.should.equal('Breakfast');
+            done();
+          });
+      })
+    })
 });
