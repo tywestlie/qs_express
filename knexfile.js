5ece5dfb13d6e03a9c79517b80f5f2f7ce932dfb
Tyler Westlie <tyler.westlie@gmail.com>
2018-08-20 13:59:19 -0600

    added production migration config

--- knexfile.js
+++ knexfile.js
@@ -13,7 +13,7 @@ module.exports = {
     },
     useNullAsDefault: true
   },
-  
+
   staging: {
     client: 'postgresql',
     connection: {
@@ -29,21 +29,13 @@ module.exports = {
       tableName: 'knex_migrations'
     }
   },
-
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      directory: './db/migrations'
+    },
+    useNullAsDefault: true
   }
 
 };
