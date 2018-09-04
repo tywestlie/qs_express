e77ed2f7521181a3b356da81cdca0d8f15417f31
Tyler Westlie <tyler.westlie@gmail.com>
2018-09-04 10:44:36 -0600

    favorite foods is outputing json correctly

--- routes/api/v1/favorite_foods.js
+++ routes/api/v1/favorite_foods.js
@@ -6,13 +6,21 @@ const configuration = require('../../../knexfile')[environment];
 const database = require('knex')(configuration);
 
 router.get('/', function(req, res, next){
-    database('foods').select()
-    .then((foods) => {
-      res.status(200).json(foods);
-    })
-    .catch((error) => {
-      res.status(500).json({ error });
+    database.raw(
+      `SELECT count(foods.id) AS timesEaten,
+       COALESCE(
+         json_agg(json_build_object('name', foods.name, 'calories', foods.calories))
+         FILTER (WHERE foods.id IS NOT NULL), '[]') AS foods
+       FROM meal_foods
+       INNER JOIN foods ON meal_foods.food_id = foods.id
+       GROUP BY foods.id
+       HAVING count(foods.id) > 0
+       ORDER BY timesEaten DESC;`
+     )
+      .then((data) => {
+        res.status(201).send(data.rows)
+      });
     });
-  });
+
 
 module.exports = router;
